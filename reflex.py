a544aaefdb382040b55abf93dcbce8a079278b67
Toby Osbourn <toby.osbourn@googlemail.com>
2014-08-11 15:17:03 +0100

    Making reflex say hello

---
+++ reflex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+from time import sleep
+import pifacecad
+
+if __name__ == "__main__":
+  cad = pifacecad.PiFaceCAD()
+
+  if "clear" in sys.argv:
+    cad.lcd.clear()
+    cad.lcd.display_off()
+    cad.lcd.backlight_off()
+  else:
+    cad.lcd.write("Hello, World")
